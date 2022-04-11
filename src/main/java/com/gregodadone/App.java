d8236420ea02f2c62becec68588512c17f853f20
Grego Dadone <grego.dadone@gmail.com>
2022-04-11 13:49:56 -0300

    First commit

---
+++ src/main/java/com/gregodadone/App.java
@@ -0,0 +1,13 @@
+package com.gregodadone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
